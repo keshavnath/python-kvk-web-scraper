869606cc11203cbbd7e00a49eb60cab4b768510f
Stephen Oostenbrink <stephen@newparadigm.nl>
2014-09-27 16:19:22 +0200

    Scrapper for www.chicagoreader.com

--- webscraping101.py
+++ webscraping101.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python
 
-print "Hello world!";
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+BASE_URL = "http://www.chicagoreader.com"
+
+def get_headline_links(section_url):
+    html_doc = urlopen(section_url).read()
+    soup = BeautifulSoup(html_doc)
+    bleaderteaser = soup.find("div", id="BleaderTeaser")
+    print bleaderteaser
+    headline_links = [li.a["href"] for li in bleaderteaser.find_all("li")]
+    return headline_links
+
+links = get_headline_links("http://www.chicagoreader.com/chicago/food-and-drink/Section?oid=846971")
+print len(links)
+for link in links:
+    print link
