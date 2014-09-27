22ce6b3f15d51ca77acbfea6aa30d7dc378cb33a
Stephen Oostenbrink <stephen@newparadigm.nl>
2014-09-27 18:13:32 +0200

    Changed to web scrape www.nu.nl

--- webscraping101.py
+++ webscraping101.py
@@ -3,17 +3,22 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
-BASE_URL = "http://www.chicagoreader.com"
+BASE_URL = "http://www.nu.nl"
 
 def get_headline_links(section_url):
     html_doc = urlopen(section_url).read()
     soup = BeautifulSoup(html_doc)
-    bleaderteaser = soup.find("div", id="BleaderTeaser")
-    print bleaderteaser
-    headline_links = [li.a["href"] for li in bleaderteaser.find_all("li")]
-    return headline_links
+    section_algemeen = soup.find("div", class_="section algemeen")
+    if section_algemeen is None:
+        print "Section not found [section algemeen]"
+    else:
+        headline_links = [BASE_URL + li.a["href"] for li in section_algemeen.find_all("li")]
+        lead_article = section_algemeen.find("div", class_="leadarticle")
+        headline_links.insert(0, BASE_URL + lead_article.h3.a["href"]) 
+        return headline_links
 
-links = get_headline_links("http://www.chicagoreader.com/chicago/food-and-drink/Section?oid=846971")
-print len(links)
-for link in links:
-    print link
+links = get_headline_links(BASE_URL)
+
+if links is not None:
+    for link in links:
+        print link
